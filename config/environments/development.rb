edcbf2271f5b0990649547d839d91db6b0514efb
Andrew Pelykh <andrew.pelykh@gmail.com>
2017-04-20 11:46:02 +0300

    Stylize pages. Configurate mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,4 +52,13 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               "gmail.com",
+    user_name:            'your_email',
+    password:             'your_password',
+    authentication:       'plain'
+  }
 end
